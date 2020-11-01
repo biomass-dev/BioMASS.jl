fbea86b8884ca00e6341cd2dc4f87bb351c14d2e
Documenter.jl <documenter@juliadocs.github.io>
2020-11-01 07:55:52 +0000

    build based on fb933f9

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BioMASS","category":"page"},{"location":"#BioMASS","page":"Home","title":"BioMASS","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BioMASS]","category":"page"}]
+}
