0a9475d93e2e11acfa64370ab9e9ab95f4698ba2
himoto <31299606+himoto@users.noreply.github.com>
2020-11-03 15:40:07 +0900

    Check optimization log

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,16 @@ using .ExecModel
 
 @testset "BioMASS.jl" begin
     optimize(MODEL_PATH, 1, max_generation=10)
+    lines = open(MODEL_PATH * "/logs/1.log", "r") do f
+        readlines(f)
+    end
+    @test lines[end][1:14] == "Generation10: "
+
     optimize_continue(MODEL_PATH, 1, max_generation=20)
+    lines = open(MODEL_PATH * "/logs/1.log", "r") do f
+        readlines(f)
+    end
+    @test lines[end][1:14] == "Generation20: "
+    
     param2biomass(MODEL_PATH)
 end
