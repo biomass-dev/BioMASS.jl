06af887cf565cd9584aa71295942f38b97613275
himoto <31299606+himoto@users.noreply.github.com>
2020-11-19 18:20:26 +0900

    Create continuation.jl

---
+++ src/continuation.jl
@@ -0,0 +1,346 @@
+using ForwardDiff: jacobian
+
+const MC = 100000           # maximum of counts
+const IVAL = 1e-2           # first variation
+const RATE = 1e-3           # variation rate
+const NEPS = 1e-12          # eps of Newton's method
+
+
+function create_diffeq(model_path::String)
+    lines::Vector{String} = []
+    open(model_path * "/set_model.jl", "r") do f
+        append!(lines, readlines(f))
+    end
+    for (i, line) in enumerate(lines)
+        if occursin("function diffeq", line)
+            lines[i] = "function diffeq2(u::Vector)\n    du = similar(u)\n\n"
+        elseif line == "end"
+            lines[i] = "    return du\nend"
+            lines = lines[1:i]
+            break
+        end
+    end
+    open(model_path * "/forwarddiff.jl", "w") do f
+        for line in lines
+            write(f, line * "\n")
+        end
+    end
+end
+
+
+# matrix transformation (large diagonal elements move to upper)
+function pivoting!(s::Matrix{Float64}, pivot::Int, MN::Int)
+    v0::Vector{Float64} = zeros(MN+1)
+    v1::Vector{Float64} = zeros(MN+1)
+    possess::Int = 0
+    max_element::Float64 = 0.0
+
+    for i in pivot:size(s,1)
+        current_element = abs(s[i, pivot])
+        if max_element <= current_element
+            max_element = current_element
+            possess = i
+        end
+    end
+
+    for j in 1:size(s,2)
+        v0[j] = s[possess, j]
+        v1[j] = s[pivot, j]
+    end
+
+    for j in 1:size(s,2)
+        s[possess, j] = v1[j]
+        s[pivot, j] = v0[j]
+    end
+end
+
+# Gaussian elimination (row reduction)
+function gaussian_elimination!(s::Matrix{Float64}, e::Vector{Float64}, MN::Int)
+    for i in 1:MN
+        pivoting!(s, i, MN)
+    end
+    # forward
+    for k in 1:size(s,1)
+        w = (s[k, k] != 0.0) ? 1.0 / s[k, k] : 1.0
+        for j in k:size(s,2)
+            s[k, j] *= w
+            for i in k:size(s,1)
+                s[i, j] -= s[i, k] * s[k, j]
+            end
+        end
+    end
+    # backward
+    for i in size(s,1):-1:1
+        sum = 0.0
+        for j in i:length(e)
+            sum += s[i, j] * e[j]
+        end
+        e[i] = s[i, end] - sum
+    end
+end
+
+
+# Newton's method
+function newtons_method!(
+        diffeq2::Function,
+        get_derivatives::Function,
+        x::Vector{Float64},
+        real_part::Vector{Float64},
+        imaginary_part::Vector{Float64}, 
+        fix_num::Int,
+        p::Vector{Float64},
+        successful::Bool,
+        BP::Int,
+        SN::Int,
+        MN::Int,
+        VN::Int)
+    u::Vector{Float64} = zeros(SN)
+    vx::Vector{Float64} = zeros(MN)
+    s::Matrix{Float64} = zeros(MN, MN+1)
+
+    for i in eachindex(x)
+        if fix_num == i
+            for j in eachindex(vx)
+                idx = i + j
+                if idx > length(x)
+                    idx -= length(x)
+                end
+                vx[j] = x[idx]
+            end
+            break
+        else
+            continue
+        end
+    end
+
+    # initial error
+    e::Vector{Float64} = zeros(MN)
+    error::Float64 = 1.0
+
+    while error > NEPS
+        for i in 1:VN
+            if fix_num == i
+                idx_param = VN - i
+                p[BP] = (idx_param == 0) ? x[fix_num] : vx[idx_param]
+                for j in eachindex(u)
+                    idx = j - i
+                    if idx == 0
+                        u[j] = x[fix_num]
+                    elseif idx < 0
+                        u[j] = vx[VN+idx]
+                    else
+                        u[j] = vx[idx]
+                    end
+                end
+                break
+            else
+                continue
+            end
+        end
+
+        # initialization
+        dFdx::Matrix{Float64} = jacobian(diffeq2, u)
+        dFdp::Vector{Float64} = get_derivatives(u, p)
+
+        F::Vector{Float64} = diffeq2(u)
+
+        eigenvalues::Array{Complex{Float64}, 1} = eigvals(dFdx)
+        for (i, eigenvalue) in enumerate(eigenvalues)
+            real_part[i] = real(eigenvalue)
+            imaginary_part[i] = imag(eigenvalue)
+        end
+
+        # s = [dF-F]
+        for i in 1:VN
+            if fix_num == i
+                for k in 1:SN
+                    for j in 1:SN
+                        idx = i + j
+                        if idx == VN
+                            s[k, j] = dFdp[k]
+                        elseif idx > VN
+                            s[k, j] = dFdx[k, idx-VN]
+                        else
+                            s[k, j] = dFdx[k, idx]
+                        end
+                    end
+                    s[k, VN] = -F[k]
+                end
+                break
+            else
+                continue
+            end
+        end
+
+        gaussian_elimination!(s, e, MN)
+
+        # update error
+        error = 0.0
+        @inbounds for i in eachindex(e)
+            vx[i] += e[i]
+            error += e[i] * e[i]
+        end
+        error = sqrt(error)
+        if isnan(error) || isinf(error)
+            successful = false
+            break
+        end
+    end
+
+    for i in eachindex(x)
+        if fix_num == i
+            for j in eachindex(vx)
+                idx = i + j
+                if idx > length(x)
+                    idx -= length(x)
+                end
+                x[idx] = vx[j]
+            end
+            break
+        else
+            continue
+        end
+    end
+end
+
+
+function new_curve!(
+        model_path::Union{String, SubString{String}},
+        p::Vector{Float64},
+        diffeq2::Function,
+        get_derivatives::Function,
+        get_steady_state::Function;
+        direction::Bool=false,
+        BP::Int, SN::Int, PN::Int=1, VN::Int = SN + 1, MN::Int = SN)
+    # BP : name(index) of bifurcation parameter
+    # SN : num of state variables
+    # PN : num of parameters
+    # VN : num of variables
+    # MN : dim of Newton's method
+    count::Int = 1
+    x::Vector{Float64} = zeros(VN)
+    dx::Vector{Float64} = zeros(VN)
+
+    real_part::Vector{Float64} = zeros(SN)
+    imaginary_part::Vector{Float64} = zeros(SN)
+
+    # file
+    if !isdir(model_path * "/data")
+        mkdir(model_path * "/data")
+    else
+        files::Vector{String} = readdir(model_path * "/data")
+        for file in files
+            rm(model_path * "/data/$file")
+        end
+    end
+    FOUT1 = open(model_path * "/data/fp.dat", "w")  # file for fixed point
+    FOUT2 = open(model_path * "/data/ev.dat", "w")  # file for eigenvalues
+
+    # initial condition
+    x[1:SN] = get_steady_state(p)
+    x[end] = p[BP]  # x-axis
+
+    # initial fixed
+    fix_val::Float64 = x[end]
+    fix_num::Int = VN
+    x[fix_num] = fix_val
+
+    # first Newton's method
+    successful::Bool = true
+    newtons_method!(
+        diffeq2, get_derivatives, x, real_part, imaginary_part, fix_num, p,
+        successful, BP, SN, MN, VN
+    )
+
+    write(FOUT1, @sprintf("%d\t", count))
+    for i in eachindex(x)
+        write(FOUT1, @sprintf("%10.8e\t", x[i]))
+    end
+    write(FOUT1, @sprintf("%d\n", fix_num))
+    write(FOUT2, @sprintf("%d\t", count))
+    for i in 1:SN
+        write(
+            FOUT2, @sprintf(
+                "%10.8e\t%10.8e\t", real_part[i], imaginary_part[i]
+            )
+        )
+    end
+    write(FOUT2, @sprintf("%10.8e\t%d\n", p[BP], fix_num))
+    count += 1
+
+    # keep optimums
+    px::Vector{Float64} = copy(x)
+
+    # variation
+    fix_val += ifelse(direction, +IVAL, -IVAL)
+
+    # same fixed variable
+    x[fix_num] = fix_val
+
+    while count <= MC && successful
+        newtons_method!(
+            diffeq2, get_derivatives, x, real_part, imaginary_part, fix_num, p,
+            successful, BP, SN, MN, VN
+        )
+
+        # maximum variation
+        for (i, prev) in enumerate(px)
+            @inbounds dx[i] = x[i] - prev
+        end
+        sum::Float64 = 0.0
+        for i in eachindex(dx)
+            @inbounds sum += dx[i] * dx[i]
+        end
+        ave::Float64 = sqrt(sum)
+        for i in eachindex(dx)
+            @inbounds dx[i] /= ave
+        end
+        px = copy(x)
+        for (i, diff) in enumerate(dx)
+            @inbounds x[i] += abs(RATE) * diff
+        end
+
+        # fix variable with maximum variation
+        fix_num = 1
+        for i in 2:length(dx)
+            if abs(dx[fix_num]) < abs(dx[i])
+                fix_num = i
+            end
+        end
+
+        # Stop calc.
+        if x[end] <= 0.0
+            successful = false
+        end
+
+        write(FOUT1, @sprintf("%d\t", count))
+        for i in eachindex(x)
+            write(FOUT1, @sprintf("%10.8e\t", x[i]))
+        end
+        write(FOUT1, @sprintf("%d\n", fix_num))
+        write(FOUT2, @sprintf("%d\t", count))
+        for i in 1:SN
+            write(
+                FOUT2, @sprintf(
+                    "%10.8e\t%10.8e\t", real_part[i], imaginary_part[i]
+                )
+            )
+        end
+        write(FOUT2, @sprintf("%10.8e\t%d\n", p[BP], fix_num))
+        count += 1
+    end
+
+    close(FOUT1)
+    close(FOUT2)
+end
+
+
+function get_bistable_regime(ev::Matrix{Float64}, SN::Int)
+    br::Vector{Int} = []
+    for i in 1:size(ev, 1)
+        if maximum(ev[i, [2j for j in 1:SN]]) > 0.0
+            push!(br, i)
+        end
+    end
+    return br
+end
