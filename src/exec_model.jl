3d72a5b36e4a9a66bf609e1001f1837107f95a11
himoto <31299606+himoto@users.noreply.github.com>
2021-01-09 13:31:05 +0900

    Add function to visualize optimized parameters

--- src/exec_model.jl
+++ src/exec_model.jl
@@ -1,6 +1,9 @@
 struct ExecModel
     path::String
+    parameters::Module
+    species::Module
     obj_func::Function
+    search_idx::Function
     search_region::Function
     gene2val::Function
     val2gene::Function
@@ -8,23 +11,34 @@ struct ExecModel
 end
 
 
-function set_model_files(dir::Union{String, SubString{String}})
+function set_model_files(dir::String)
     for child in readdir(dir)
         # if isdir("$dir/$child")
         if child == "name2idx"
-            set_model_files("$dir/$child")
+            set_model_files(
+                joinpath(
+                    "$dir", "$child"
+                )
+            )
         elseif splitext(child)[end] == ".jl"
-            include("$dir/$child")
+            include(
+                joinpath(
+                    "$dir", "$child"
+                )
+            )
         end
     end
 end
 
 
 function load_model(model_path::String)
-    set_model_files(strip(model_path, '/'))
+    set_model_files(model_path)
     return ExecModel(
         model_path,
+        C,
+        V,
         objective,
+        get_search_index,
         get_search_region,
         decode_gene2val,
         encode_val2gene,
