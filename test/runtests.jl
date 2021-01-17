21d3ab49fb763912f49aa16a8340dfdd7d4bc32c
himoto <31299606+himoto@users.noreply.github.com>
2021-01-17 14:48:30 +0900

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,7 @@ using Test
 @time begin
     @testset "BioMASS.jl" begin
         include("parameter_estimation.jl")
+        # include("dde_simulation.jl")
         include("bifurcation_analysis.jl")
     end
 end
