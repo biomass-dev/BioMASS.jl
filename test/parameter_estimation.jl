90df4c92bef272f99fff7996bffbbc2fdf9368e1
himoto <31299606+himoto@users.noreply.github.com>
2022-10-28 23:02:16 +0100

    Test scipy_differential_evolution

--- test/parameter_estimation.jl
+++ test/parameter_estimation.jl
@@ -1,52 +1,19 @@
 import BioMASS: isinstalled
+using PyCall
 
 @testset "Parameter Estimation" begin
     model_ode = Model("../examples/fos_model")
     output = []
     @testset "optimization" begin
-        optimize(
-            model_ode, 1, max_generation=3, popsize=3,
-            local_search_method="mutation", n_children=15
-        )
+        # initpop = generate_initial_population(model_ode);
+        optimizer_options = Dict{String, Int}(py"{'maxiter': 10}");
+        scipy_differential_evolution(model_ode, 1, optimizer_options);
         lines = open(joinpath(model_ode.path, "logs", "1.log"), "r") do f
             readlines(f)
         end
-        @test lines[end][1:13] == "Generation3: "
+        @test startswith(lines[end - 1], "differential_evolution step 10:")
         push!(output, "logs")
         push!(output, "fitparam")
-    end
-    @testset "optimization_continue" begin
-        optimize_continue(
-            model_ode, 1, max_generation=6, popsize=3,
-            local_search_method="CMAES", maxiter=30
-        )
-        lines = open(joinpath(model_ode.path, "logs", "1.log"), "r") do f
-            readlines(f)
-        end
-        @test lines[end][1:13] == "Generation6: "
-        # test differential_evolution
-        if isinstalled("scipy.optimize")
-            @testset "Differential evolution" begin
-                optimize_continue(
-                    model_ode, 1, max_generation=9, popsize=3,
-                    local_search_method="DE", maxiter=10
-                )
-                lines = open(joinpath(model_ode.path, "logs", "1.log"), "r") do f
-                    readlines(f)
-                end
-                @test lines[end][1:13] == "Generation9: "
-            end
-            @testset "Modified Powell's method" begin
-                optimize_continue(
-                    model_ode, 1, max_generation=12, popsize=3,
-                    local_search_method="Powell", maxiter=5
-                )
-                lines = open(joinpath(model_ode.path, "logs", "1.log"), "r") do f
-                    readlines(f)
-                end
-                @test lines[end][1:14] == "Generation12: "
-            end
-        end
     end
     if isinstalled("matplotlib")
         @testset "visualization" begin
